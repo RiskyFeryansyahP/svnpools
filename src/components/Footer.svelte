b0f47f8821d083920ccaeaba7f0109b318138c7a
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-09-23 11:42:24 +0700

    add footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+    <div class="copyright"> Copyright 2020 confus1on </div>
+</footer>
+
+<style>
+    footer {
+        padding: 40px;
+        text-align: center;
+    }
+
+    .copyright {
+        color: #aaaaaa;
+        font-size: 14px;
+        display: inline-block;
+        padding: 20px;
+        border-top: 1px solid #dddddd;
+    }
+</style>
