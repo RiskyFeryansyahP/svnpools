aef4ea367c252501321db0eda40a312f02c75684
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-09-24 03:23:22 +0700

    add basic validation form

--- src/components/CreatePoolForm.svelte
+++ src/components/CreatePoolForm.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     // importing
     import Button from '../shared/Button.svelte'
 
@@ -7,9 +7,46 @@
         answerA: '',
         answerB: '',
     }
+    let errors = {
+        question: '',
+        answerA: '',
+        answerB: '',
+    }
+    let valid = false
+
+    const validationForm = (): boolean => {
+        valid  = true
+
+        if (fields.question.trim().length < 5) {
+        errors.question = 'Field question length must greater than 5!'
+        valid = false
+        } else {
+            errors.question = ''
+        }
+
+        if (fields.answerA.trim().length < 1) {
+            errors.answerA = 'Field AnswerA cant not be empty!'
+            valid = false
+        } else {
+            errors.answerA = ''
+        }
+
+        if (fields.answerB.trim().length < 1) {
+            errors.answerB = 'Field AnswerB cant not be empty!'
+            valid = false
+        } else {
+            errors.answerB = ''
+        }
+
+        return valid
+    }
 
     const handleSubmitPool = () => {
-        console.log(fields)
+        const valid = validationForm()
+
+        if (valid) {
+            console.log(fields)
+        }
     }
 </script>
 
@@ -17,14 +54,17 @@
     <div class="form-field">
         <label for="question">Pool Question:</label>
         <input type="text" id="question" bind:value={fields.question}>
+        <div class="error"> {errors.question} </div>
     </div>
     <div class="form-field">
         <label for="answer-a">Answer A:</label>
         <input type="text" id="answer-a" bind:value={fields.answerA}>
+        <div class="error"> {errors.answerA} </div>
     </div>
     <div class="form-field">
         <label for="answer-b">Answer B:</label>
         <input type="text" id="answer-b" bind:value={fields.answerB}>
+        <div class="error"> {errors.answerB} </div>
     </div>
     <Button type="secondary" flat={true}>Add Pool</Button>
 </form>
@@ -48,5 +88,11 @@
     label {
         margin: 10px auto;
         text-align: left;
+    }
+
+    .error {
+        font-weight: bold;
+        font-size: 12px;
+        color: #d91b42;
     }
 </style>
