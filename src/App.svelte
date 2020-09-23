47769c40efb7a2be27b3a08336159b8040412f43
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-09-23 11:42:34 +0700

    load all component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
+	//imporint
+	import Header from './components/Header.svelte'
+	import Footer from './components/Footer.svelte'
+	import Tabs from './shared/Tabs.svelte'
+	import PoolForm from './components/CreatePoolForm.svelte'
+
+	// tabs
+	let items = ['Current Pools', 'Add New Pools']
+	let activeItem = 'Current Pools'
+
+	const tabChange = (e: CustomEvent) => {
+		activeItem = e.detail
+	}
 </script>
 
+<Header />
 <main>
-	<h1> Hello, Ninjas </h1>
+	<Tabs {activeItem} {items} on:tabChange={tabChange} />
+	{#if activeItem === 'Current Pools'}
+		<p> Pool list component goes here </p>	
+	{:else if activeItem === 'Add New Pools'}
+		<PoolForm />
+	{/if}
 </main>
+<Footer />
 
 <style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
 </style>
