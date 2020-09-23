2ed96640e1ef99c3f03e20103757b901afac57a6
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-09-23 11:42:30 +0700

    add header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<header>
+    <h1>
+        <img src="/images/pool_ninja_logo.svg" alt="Pool Ninja Logo">
+    </h1>
+</header>
+
+<style>
+    header {
+        background: #f7f7f7;
+        padding: 20px;
+    }
+
+    h1 {
+        margin: 0;
+        text-align: center;
+    }
+
+    img {
+        max-width: 190px;
+    }
+</style>
